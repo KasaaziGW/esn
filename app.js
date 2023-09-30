44c062c8593f7892ce502908a62434417be47461
KasaaziGW <kasaazi.gw@gmail.com>
2023-09-30 11:31:54 +0300

    Added initial login page and it's backend code

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.set("view engine", "ejs"); // specifying the templating engine
 //
 // connecting to the database
 const dbUrl = "mongodb+srv://umumis:umu123@cluster0.odksibj.mongodb.net/";
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl, (err) => {
+  if (err) console.log(`Couldn't connect to MongoDB \n${err}.`);
+  else console.log("Succesfully connected to MongoDB.");
+});
+
+// loading the login page when the user visits the '/'
+app.get("/", (request, response) => {
+  response.render("login");
+});
 
 // starting the server
 app.listen(PORT, () => {
