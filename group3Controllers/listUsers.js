a05c55f832cb695ca72b48731007b4986bed3790
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2023-11-28 22:00:32 +0300

    list the users as gotten from the database
    Co-authored-by: nasracheal <nasracheal@users.noreply.github.com>

--- group3Controllers/listUsers.js
+++ group3Controllers/listUsers.js
@@ -1,5 +1,5 @@
 // list the users that have registered on the platform
-
+const {Citizen}  =require ('../models/Citizen')
 async function listUsers(request, response){
     session = request.session;
   uname = request.session.fullname;
@@ -7,19 +7,20 @@ async function listUsers(request, response){
     // check if the user is an administrator
 
     // fetch the list of users from the database
-
+    const users = await Citizen.find()
     // return the list for rendering
     response.render('listUsers',  {
         data: {
           userid: request.session.userId,
           fullname: request.session.fullname,
         },
-        users: [
-            {fullName: 'Esau Lwanga', privilege: 'Admin', username: 'x'},
-            {fullName: 'Hillary Tumukunde', privilege: 'Admin', username: 'x'},
-            {fullName: 'Andrew Kataate', privilege: 'Admin', username: 'x'},
-            {fullName: 'Racheal Nasasira', privilege: 'Admin', username: 'x'}
-        ]
+        // users: [
+        //     {fullName: 'Esau Lwanga', privilege: 'Admin', username: 'x'},
+        //     {fullName: 'Hillary Tumukunde', privilege: 'Admin', username: 'x'},
+        //     {fullName: 'Andrew Kataate', privilege: 'Admin', username: 'x'},
+        //     {fullName: 'Racheal Nasasira', privilege: 'Admin', username: 'x'}
+        // ],
+        users
       })
 }
 
