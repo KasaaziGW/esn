d15020e802226b19129a4a5305eca3d1ca9b4f20
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2023-12-13 19:22:10 +0300

    log out the user after 15 seconds

--- public/js/deactivatetracker.js
+++ public/js/deactivatetracker.js
@@ -2,5 +2,10 @@ var socket = io();
 var _id = document.querySelector("#_id").textContent;
 var eventId = `deactivated_${_id}`
 socket.on(eventId, () => {
-    alert("Your account has been deactivated. Please contact administrator")
+    alert("Your account has been deactivated. Please contact administrator");
+    window.setTimeout(logout(), 15000); // logout the user after 15 seconds
 });
+
+function logout() {
+    window.location.replace('http://localhost:4000/logout');
+}
