486f1b6467d326e54ab705de5ef83177901a0473
Esau Lwanga <31913619+lemndev@users.noreply.github.com>
2023-12-14 17:24:19 +0300

    indicate users who are online

--- group3Controllers/listUsers.js
+++ group3Controllers/listUsers.js
@@ -3,6 +3,21 @@ const {Citizen}  =require ('../models/Citizen')
 async function listUsers(request, response){
     // fetch the list of users from the database
     const users = await Citizen.find()
+
+    // check for users who are currently logged in
+    const activeSessions = request.sessionStore.sessions
+    const activeUsers = []
+    for (let session in activeSessions){
+        const sessionObject = JSON.parse(activeSessions[session])
+        activeUsers.push(sessionObject.userId)
+    }
+    // construct the list of users indicating those who are currently logged in
+    let usersList = []
+    for (let user of users){
+      const online = activeUsers.includes(user.username)
+      usersList.push({...user._doc, online})
+    }
+
     // return the list for rendering
     response.render('listUsers',  {
         data: {
@@ -11,7 +26,7 @@ async function listUsers(request, response){
           privilege: request.session.privilege,
           _id: request.session._id
         },
-        users
+        users: usersList
       })
 }
 
